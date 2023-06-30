f48eb2484c3d8df77b03d099121894743502fe69
Aleksand Muzhev <muzhev1990@mail.ru>
2023-06-30 20:32:25 +0500

    Added Tests

---
+++ src/test/java/ru/netology/web/data/User.java
@@ -0,0 +1,16 @@
+package ru.netology.web.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String login;
+    private String password;
+    private String status;
+}
